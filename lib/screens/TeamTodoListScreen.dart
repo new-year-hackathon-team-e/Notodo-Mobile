a3083a2c01a7eefb5b62f10e09c0bdd28a2e965a
sb8919 <sb8919@naver.com>
2024-01-13 00:26:02 +0900

    feat(TeamTodoListScreen) : TeamTodoListScreen UI구성

--- lib/screens/TeamTodoListScreen.dart
+++ lib/screens/TeamTodoListScreen.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:notodo/common/layout/DefaultLayout.dart';
 
-class TeamTodoLIstScreen extends StatelessWidget {
-  const TeamTodoLIstScreen({super.key});
+import '../components/Card/CardWidget.dart';
+
+class TeamTodoListScreen extends StatefulWidget {
+  const TeamTodoListScreen({super.key});
+
+  @override
+  State<TeamTodoListScreen> createState() => _TeamTodoListScreenState();
+}
+
+class _TeamTodoListScreenState extends State<TeamTodoListScreen> {
+
+  Future<String> future = Future<String>.delayed(
+    const Duration(seconds: 2),
+        () => 'Data Loaded',
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      child: Center(
-        child: Text('team'),
-      ),
+    return DefaultLayout(
+      title: 'Team 신청 리스트',
+      child: Center(child: Column(
+        children: [
+          FutureBuilder(
+            future: future,
+            builder: ((context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return Expanded(
+                  child: ListView.separated(
+                    separatorBuilder: ((context, index) => const SizedBox(
+                      height: 5.0,
+                    )),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      final pItem = snapshot.data![index];
+                      return GestureDetector(
+                        onTap: () {},
+                        child: const TodoCardHorizontal(
+                          TodoTeamTitle: '금연방',
+                          TodoTeamJoinMember: '3',
+                          TodoTeamTotalMember: '5',
+                          iconText: '🚬',
+                          backgroundColor: Colors.grey,
+                          TodoTeamCategoroy: '건강',
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+            }),
+          ),
+        ],
+      )),
     );
   }
 }
