f817db0485447fb14b979ddf5daede3c798d37f0
younha00 <ayeon0@office.skhu.ac.kr>
2024-01-13 05:21:10 +0900

    오탈자 수정

--- lib/screens/detailCategoryListScreen.dart
+++ lib/screens/detailCategoryListScreen.dart
@@ -9,30 +9,34 @@ class DetailCategoryListScreen extends StatefulWidget {
   const DetailCategoryListScreen({super.key});
 
   @override
-  State<DetailCategoryListScreen> createState() => _DetailCategoryListScreenState();
+  State<DetailCategoryListScreen> createState() =>
+      _DetailCategoryListScreenState();
 }
 
 class _DetailCategoryListScreenState extends State<DetailCategoryListScreen> {
-
   Future<String> future = Future<String>.delayed(
     const Duration(seconds: 2),
-        () => 'Data Loaded',
+    () => 'Data Loaded',
   );
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: '건강 '+'카테고리',
+      title: '건강 ' '카테고리',
       floatingActionButton: FloatingActionButton(
         backgroundColor: PRIMARY_COLOR,
         tooltip: '팀원 모집하기',
-        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => TeamRecruitScreen())); },
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => TeamRecruitScreen()));
+        },
         child: const Icon(
           Icons.edit,
           color: Colors.white,
         ),
       ),
-      child: Center(child: Column(
+      child: Center(
+          child: Column(
         children: [
           FutureBuilder(
             future: future,
@@ -45,8 +49,8 @@ class _DetailCategoryListScreenState extends State<DetailCategoryListScreen> {
                 return Expanded(
                   child: ListView.separated(
                     separatorBuilder: ((context, index) => const SizedBox(
-                      height: 5.0,
-                    )),
+                          height: 5.0,
+                        )),
                     itemCount: 3,
                     itemBuilder: (context, index) {
                       final pItem = snapshot.data![index];
@@ -58,7 +62,7 @@ class _DetailCategoryListScreenState extends State<DetailCategoryListScreen> {
                           TodoTeamTotalMember: '5',
                           iconText: '🚬',
                           backgroundColor: Colors.grey,
-                          TodoTeamCategoroy: '건강',
+                          TodoTeamCategory: '건강',
                         ),
                       );
                     },
